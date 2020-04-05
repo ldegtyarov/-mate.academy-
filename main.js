c55a3b4b065cb6b76120a88363c592c0ef91ce53
ldegtyarov <l.dehtyarov@gmail.com>
2020-04-05 10:43:47 +0300

    New

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+let a = 2 + 2;
+switch (a) {
+   case 3:
+      alert('Not Enough');
+      break;
+   case 4:
+      alert('Yes, It is!');
+      break;
+   case 5:
+alert('Too many');
+break;
+default:
+alert('There are no such values');
+}
